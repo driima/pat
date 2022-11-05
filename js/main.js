97dfe99ebcef0cfe3a5112ec784ac285a1a4c7b0
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 13:20:07 +0000

    Initial

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("pat").addEventListener("mousedown", function() {
+    document.getElementById("pat").src = "img/pat2.png";
+});
+
+document.getElementById("pat").addEventListener("mouseup", function() {
+    document.getElementById("pat").src = "img/pat1.png";
+});
