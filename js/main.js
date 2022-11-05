22229179cdc3896acec9c5a7a41bba6c9aca139c
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 17:51:50 +0000

    Remove lang and fix other stuff

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ document.getElementById("pat").addEventListener("mousedown", function() {
 document.getElementById("pat").addEventListener("mouseup", function() {
     document.getElementById("pat").src = "img/pat1.png";
 });
+
+document.getElementById("pat").addEventListener("touchstart", function() {
+    document.getElementById("pat").src = "img/pat2.png";
+});
+
+document.getElementById("pat").addEventListener("touchend", function() {
+    document.getElementById("pat").src = "img/pat1.png";
+});
